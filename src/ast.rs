94724729c7fd29ff44acda934f0a0dbac3c04b61
David Galdamez <david.galdamez2014@gmail.com>
2026-07-04 08:00:34 -0600

    feat: started formatting statements

--- src/ast.rs
+++ src/ast.rs
@@ -7,11 +7,11 @@ pub trait Node {
     fn as_any(&self) -> &dyn Any;
 }
 
-pub trait Statement: Node + fmt::Debug {
+pub trait Statement: Node + fmt::Debug + fmt::Display {
     fn statement_node(&self);
 }
 
-trait Expression: Node + fmt::Debug + Default {
+trait Expression: Node + fmt::Debug + fmt::Display {
     fn expression_node(&self);
 }
 
@@ -19,6 +19,16 @@ trait Expression: Node + fmt::Debug + Default {
 pub struct Program {
     // We use a Box<dyn Statement> because we can have a lot of Statement implementation
     pub statements: Vec<Box<dyn Statement>>,
+}
+
+impl fmt::Display for Program {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for stmt in &self.statements {
+            write!(f, "{}", stmt)?;
+        }
+
+        Ok(())
+    }
 }
 
 impl Node for Program {
@@ -40,6 +50,18 @@ pub struct LetStatement {
     pub token: token::Token, // token::LET token
     pub name: Identifier,
     pub value: Identifier,
+}
+
+impl fmt::Display for LetStatement {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} ", self.token_literal())?;
+        write!(f, "{}", self.name)?;
+        write!(f, " = ")?;
+
+        if self.value {
+            write!(f, "{}", self.value)?;
+        }
+    }
 }
 
 impl Node for LetStatement {
@@ -77,9 +99,35 @@ impl Statement for ReturnStatement {
 }
 
 #[derive(Debug, Default)]
+pub struct ExpressionStatement {
+    token: token::Token,
+    expression: Identifier,
+}
+
+impl Node for ExpressionStatement {
+    fn token_literal(&self) -> &str {
+        &self.token.literal
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl Statement for ExpressionStatement {
+    fn statement_node(&self) {}
+}
+
+#[derive(Debug, Default)]
 pub struct Identifier {
     pub token: token::Token, // token::IDENT token
     pub value: String,
+}
+
+impl fmt::Display for Identifier {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.value)
+    }
 }
 
 impl Node for Identifier {
