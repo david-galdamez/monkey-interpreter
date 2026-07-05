fcca677b4443436b7ddd0f193f38150534499ab0
David Galdamez <david.galdamez2014@gmail.com>
2026-07-05 14:37:18 -0600

    feat: implemented parsing, integers and literal expressions

--- src/parser.rs
+++ src/parser.rs
@@ -1,20 +1,34 @@
 use std::{collections::HashMap, mem};
 
 use crate::{
-    ast::{Expression, Identifier, LetStatement, Program, ReturnStatement, Statement},
+    ast::{
+        Expression, ExpressionStatement, Identifier, IntegerLiteral, LetStatement, Program,
+        ReturnStatement, Statement,
+    },
     lexer, token,
 };
 
-type prefix_parse_fn = fn() -> Box<dyn Expression>;
-type infix_parse_fn = fn(Box<dyn Expression>) -> Box<dyn Expression>;
+#[derive(Debug, Ord, PartialEq, PartialOrd, Eq)]
+enum ExpressionConstants {
+    LOWEST,
+    EQUALS,
+    LESSGREATER,
+    SUM,
+    PRODUCT,
+    PREFIX,
+    CALL,
+}
+
+type PrefixParseFns = fn(&mut Parser) -> Option<Box<dyn Expression>>;
+type InfixParseFns = fn(&mut Parser, Box<dyn Expression>) -> Option<Box<dyn Expression>>;
 
 pub struct Parser<'a> {
     lex: lexer::Lexer<'a>,
     cur_token: token::Token,
     peek_token: token::Token,
 
-    prefix_parse_fns: HashMap<token::TokenType, prefix_parse_fn>,
-    infix_parse_fns: HashMap<token::TokenType, infix_parse_fn>,
+    prefix_parse_fns: HashMap<token::TokenType, PrefixParseFns>,
+    infix_parse_fns: HashMap<token::TokenType, InfixParseFns>,
 
     errors: Vec<String>,
 }
@@ -31,6 +45,9 @@ impl<'a> Parser<'a> {
 
             errors: Vec::new(),
         };
+
+        parser.register_prefix(token::IDENT, Parser::parse_identifier);
+        parser.register_prefix(token::INT, Parser::parse_integer_literal);
 
         parser.next_token();
         parser.next_token();
@@ -65,11 +82,13 @@ impl<'a> Parser<'a> {
         Some(program)
     }
 
+    // Statements
+
     fn parse_statement(&mut self) -> Option<Box<dyn Statement>> {
         match self.cur_token.token_type {
             token::LET => self.parse_let_statement(),
             token::RETURN => self.parse_return_statement(),
-            _ => None,
+            _ => self.parse_expression_statement(),
         }
     }
 
@@ -116,6 +135,61 @@ impl<'a> Parser<'a> {
         Some(Box::new(stmt))
     }
 
+    fn parse_expression_statement(&mut self) -> Option<Box<dyn Statement>> {
+        let mut stmt = ExpressionStatement {
+            token: self.cur_token.clone(),
+            ..Default::default()
+        };
+
+        stmt.expression = self.parse_expression(ExpressionConstants::LOWEST);
+
+        if self.peek_token_is(token::SEMICOLON) {
+            self.next_token();
+        }
+
+        Some(Box::new(stmt))
+    }
+
+    // Expressions
+
+    fn parse_identifier(parser: &mut Parser) -> Option<Box<dyn Expression>> {
+        Some(Box::new(Identifier {
+            token: parser.cur_token.clone(),
+            value: parser.cur_token.literal.clone(),
+        }))
+    }
+
+    fn parse_integer_literal(parser: &mut Parser) -> Option<Box<dyn Expression>> {
+        let mut literal = IntegerLiteral {
+            token: parser.cur_token.clone(),
+            ..Default::default()
+        };
+
+        let value: i64 = match parser.cur_token.literal.parse() {
+            Ok(val) => val,
+            Err(_) => {
+                parser.errors.push(format!(
+                    "could not parse {} as integer",
+                    parser.cur_token.literal
+                ));
+                return None;
+            }
+        };
+        literal.value = value;
+
+        Some(Box::new(literal))
+    }
+
+    fn parse_expression(&mut self, precedence: ExpressionConstants) -> Option<Box<dyn Expression>> {
+        match self.prefix_parse_fns.get(self.cur_token.token_type) {
+            Some(pref) => {
+                let left_exp = pref(self);
+                left_exp
+            }
+            None => None,
+        }
+    }
+
     fn cur_token_is(&self, tok: token::TokenType) -> bool {
         self.cur_token.token_type == tok
     }
@@ -134,11 +208,11 @@ impl<'a> Parser<'a> {
         }
     }
 
-    fn register_prefix(&mut self, tok: token::TokenType, func: prefix_parse_fn) {
+    fn register_prefix(&mut self, tok: token::TokenType, func: PrefixParseFns) {
         self.prefix_parse_fns.insert(tok, func);
     }
 
-    fn register_infix(&mut self, tok: token::TokenType, func: infix_parse_fn) {
+    fn register_infix(&mut self, tok: token::TokenType, func: InfixParseFns) {
         self.infix_parse_fns.insert(tok, func);
     }
 }
@@ -146,7 +220,10 @@ impl<'a> Parser<'a> {
 #[cfg(test)]
 mod tests {
     use crate::{
-        ast::{ExpressionStatement, LetStatement, Node, ReturnStatement, Statement},
+        ast::{
+            ExpressionStatement, Identifier, IntegerLiteral, LetStatement, Node, ReturnStatement,
+            Statement,
+        },
         lexer::Lexer,
         parser::Parser,
     };
@@ -310,5 +387,91 @@ mod tests {
         );
 
         let stmt = type_stmt.unwrap();
+
+        let exp = match &stmt.expression {
+            Some(expression) => {
+                let exp = expression.as_any();
+                exp.downcast_ref::<Identifier>()
+            }
+            None => {
+                panic!("no expression found");
+            }
+        };
+
+        assert!(
+            exp.is_some(),
+            "statement.expression is not an identifier expression"
+        );
+        let exp = exp.unwrap();
+
+        assert!(
+            exp.value == "foobar",
+            "ident.value not foobar, got = {}",
+            exp.value
+        );
+
+        assert!(
+            exp.token_literal() == "foobar",
+            "ident.token_literal not foobar, got = {}",
+            exp.value
+        );
+    }
+
+    #[test]
+    fn test_integer_literal() {
+        let input = "5;";
+
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+        let program = parser.parse_program();
+        check_parse_errors(&parser);
+
+        assert!(program.is_some(), "parse_program() returned none");
+
+        let program = program.unwrap();
+        assert!(
+            program.statements.len() == 1,
+            "program.statements does not contain enough statements, got = {}",
+            program.statements.len()
+        );
+
+        let type_stmt = program.statements[0]
+            .as_any()
+            .downcast_ref::<ExpressionStatement>();
+
+        assert!(
+            type_stmt.is_some(),
+            "program.statement[0] is not an expression statement"
+        );
+
+        let stmt = type_stmt.unwrap();
+
+        let exp = match &stmt.expression {
+            Some(expression) => {
+                let exp = expression.as_any();
+                exp.downcast_ref::<IntegerLiteral>()
+            }
+            None => {
+                panic!("no expression found");
+            }
+        };
+
+        assert!(
+            exp.is_some(),
+            "statement.expression is not an identifier expression"
+        );
+        let exp = exp.unwrap();
+
+        assert!(
+            exp.value == 5,
+            "ident.value not foobar, got = {}",
+            exp.value
+        );
+
+        assert!(
+            exp.token_literal() == "5",
+            "ident.token_literal not 5, got = {}",
+            exp.value
+        );
     }
 }
